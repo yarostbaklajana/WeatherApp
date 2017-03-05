2aa9a2431d25c495041c820e9d5e6ade8215169c
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-05 11:39:48 +0200

    Added production script, deploy,sh script moved to scripts folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-    context: path.join(__dirname, '/src'),
-    entry: './index',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    resolve: {
+module.exports = function (env) {
+    const isProd = env === "prod";
+    if (isProd) {
+        process.env.NODE_ENV = "production";
+    }
+    const config = {
+        context: path.join(__dirname, '/src'),
+        entry: './index',
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js'
+        },
+        resolve: {
             extensions: ['.jsx', '.js']
         },
 
@@ -30,7 +36,10 @@ module.exports = {
                     test: /\.css$/,
                     use: ExtractTextWebpackPlugin.extract({
                         use: {
-                            loader: 'css-loader'
+                            loader: 'css-loader',
+                            options: {
+                                minimize: isProd
+                            }
                         }
                     })
                 }
@@ -47,5 +56,22 @@ module.exports = {
                 filename: "bundle.css"
             })
         ]
+    }
+
+    if (isProd) {
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        )
+    }
+    return config;
 
 }
