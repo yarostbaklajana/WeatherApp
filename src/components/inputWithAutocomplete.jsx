eb6f690f8b83d242cb64a1f63ddf8eff6efd9327
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-12 12:47:44 +0200

    Added backdrop

--- src/components/inputWithAutocomplete.jsx
+++ src/components/inputWithAutocomplete.jsx
@@ -1,41 +1,70 @@
 import React from 'react';
-import * as locationsArr from '../../locations/ukraine.json';
 
-class InputWithAutocomplete extends React.PureComponent {
+class InputWithAutocomplete extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
-            coincidenses: []
+            hints: [],
+            isVisibleHints: false
         }
         this.handleChange = this.handleChange.bind(this);
+        this.fillInputWithCheckedHint = this.fillInputWithCheckedHint.bind(this);
+        this.closeHints = this.closeHints.bind(this);
     }
 
     handleChange(event) {
-
         let inputValue = event.target.value;
         this.setState({
             value: inputValue,
-            coincidenses: this.findCoincidence(inputValue)
+            hints: this.findHints(inputValue),
+            isVisibleHints: this.state.hints !== 0
         });
-        console.log(this.state.coincidenses);
+
+        this.props.onChange(event.target.value);
     }
 
-    findCoincidence(part) {
-        let coincidenses = locationsArr.filter((location) => location.name.startsWith(part));
-        return coincidenses;
+    findHints(part) {
+        let hints = (part === '') ? [] : this.props.locations.filter((location) => location.name.toUpperCase().startsWith(part.toUpperCase())).slice(0, 10);
+        return hints;
+    }
+
+    fillInputWithCheckedHint(hint, event) {
+        let checkedValue = hint.name;
+
+        this.setState({
+            value: checkedValue,
+            hints: [],
+            isVisibleHints: false
+        });
+
+        this.props.onChange(checkedValue);
+
+
+        var callEvent = new Event('onChange', { bubbles: true });
+        event.target.dispatchEvent(callEvent);
+    }
+
+    closeHints() {
+        this.setState({
+            hints: [],
+            isVisibleHints: false
+        });
     }
 
     render() {
-        let coincidensesList = this.state.coincidenses.map((coincidense) => <li key={coincidense.id}>{coincidense.name}</li>);
+        let hintsList = this.state.hints.map((hint) => <li className='locations-hints_hint' onClick={(evt) => this.fillInputWithCheckedHint(hint, evt)} name={hint.name} key={hint.id}>{hint.name}</li>);
+        const backdrop = this.state.isVisibleHints ? <div className='backdrop' onClick={this.closeHints}></div> : null;
         return (
-            <div className='input-container'>
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                <ul>{coincidensesList}</ul>
+            <div>
+                {backdrop}
+                <div className='input-container'>
+                    <input className='input-with-autocomplete' type="text" value={this.state.value} onChange={this.handleChange} />
+                    <ul className='locations-hints'>{hintsList}</ul>
+                </div>
             </div>
         );
     }
-
 }
 
 export default InputWithAutocomplete;
