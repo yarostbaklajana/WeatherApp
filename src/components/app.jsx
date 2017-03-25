fc031b7b48e525afc7a3b61514260ca239c63635
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-25 19:11:41 +0200

    Created method for converting pressure and compass direction, imported momentJS to format date and time

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchForm from './searchForm';
 import axios from 'axios';
+import moment from 'moment';
 
 class App extends React.Component {
     constructor(props) {
@@ -10,7 +11,12 @@ class App extends React.Component {
     }
 
     async handleSubmit(cityID) {
-        console.log(await this.getForecasts(cityID));
+        try {
+            console.log(await this.getForecasts(cityID));
+        } catch (err) {
+            console.log(err);
+        }
+        
     }
 
     async getForecasts(cityID) {
@@ -27,38 +33,54 @@ class App extends React.Component {
 
             return this.getForecastCards(threeDayForecastList);
         } catch (err) {
-            return err.message;
+            throw err.message;
         }
     }
 
     getForecastCards(allForecastedDataArray) {
         let forecastCards = [];
         allForecastedDataArray.forEach((singleForecast) => { forecastCards.push({
-                date_time: this.convertToLocaleDateTime(singleForecast.dt),
+                dateTime: this.convertToLocaleDateTime(singleForecast.dt),
                 weather: singleForecast.weather[0].description,
-                weather_icon_id: singleForecast.weather[0].id,
+                weatherIconId: singleForecast.weather[0].id,
                 temperature: singleForecast.main.temp,
                 humidity: singleForecast.main.humidity,
                 pressure: singleForecast.main.pressure,
                 wind: {
-                    direction: singleForecast.wind.deg,
-                    speed: singleForecast.wind.speed
+                    direction: this.convertToCompassDirection(singleForecast.wind.deg),
+                    speed: singleForecast.wind.speed.toFixed(1)
                 }
             }) });
             return forecastCards;
     }
 
     convertToLocaleDateTime(utc) {
-        let date = new Date(0);
-        date.setUTCSeconds(utc);
-        const yy = date.getFullYear();
-        const mm = date.getMonth();
-        const dd = date.getDay();
-        return dd + '/' + mm + '/' + yy  + ' ' + date.toLocaleTimeString({ hour12: false });
+        const dateTime = moment.unix(utc);
+        return dateTime.format('DD/MM/YYYY - HH:mm'); 
     }
 
-    convertToCompassDirection(directionInPercents) {
-        
+    convertHectopascalsToMmHg(hPa) {
+        return hpa * 0,75;
+    }
+
+    convertToCompassDirection(directionInDeg) {
+        if (directionInDeg >= 337.2 && directionInDeg <= 22.5) {
+            return 'N';
+        } else if (directionInDeg > 22.5 && directionInDeg < 67.5) {
+            return 'NE';
+        } else if (directionInDeg >= 67.5 && directionInDeg <= 112.5) {
+            return 'E';
+        } else if (directionInDeg > 112.5 && directionInDeg < 157.5) {
+            return 'SE';
+        } else if (directionInDeg >= 157.5 && directionInDeg <= 202.5) {
+            return 'S';
+        } else if (directionInDeg > 202.5 && directionInDeg < 247.5) {
+            return 'SW';
+        } else if (directionInDeg >= 247.5 && directionInDeg <= 292.5) {
+            return 'W';
+        } else {
+            return 'NW';
+        }
     }
 
     render() {
