1257cc8483621b0f552bb5e1f7183223854c16dd
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 14:12:58 +0300

    Added upstream configuration to checkout command

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -5,6 +5,6 @@ set -o errexit #abort if any command fails
 mkdir build
 cd build
 git clone https://github.com/yarostbaklajana/WeatherApp.git
-git checkout gh-pages
+git checkout -b gh-pages --track origin/gh-pages
 rm -type f
 rm !(.git) -type c
