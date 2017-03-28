eaa38688730a6c9fdb409013c8e4378343267c89
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 14:29:37 +0300

    Changed option again

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -5,6 +5,6 @@ set -o errexit #abort if any command fails
 mkdir build
 cd build
 git clone https://github.com/yarostbaklajana/WeatherApp.git
-git checkout -b gh-pages --track origin/gh-pages
+git checkout gh-pages 
 rm -type f
 rm !(.git) -type c
