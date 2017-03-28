73058e45dcf93fc918f08156c1e68fa14d41ff07
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 16:44:20 +0300

    Fixed pre-deploy script

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -o errexit #abort if any command fails
-if [$TRAVIS_PULL_REQUEST != true || $TRAVIS_BRANCH != gh-pages]
+if [[ $TRAVIS_PULL_REQUEST != true ]] || [[ $TRAVIS_BRANCH != gh-pages ]]
 then 
     mkdir build
     cd build
     git clone https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/WeatherApp.git .
     git checkout gh-pages
     shopt -s extglob 
-    rm -rf !(.git)
+    ls | grep -v .git | xargs rm -rf
     cd ..
 else 
     echo "Skipping a pre-deployment with the script provider because the current build is a pull request or gh-pages."
+fi
