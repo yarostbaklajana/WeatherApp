767b5639f9105279cf538eaeadc1e2eca71de3f3
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 16:17:15 +0300

    Added pre-deployment script configuration and fixed deployment script

--- scripts/before_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit #abort if any command fails
-
-mkdir build
-cd build
-git clone https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/WeatherApp.git .
-git checkout gh-pages
-
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,13 @@
 
 set -o errexit #abort if any command fails
 
-REPO=https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/WeatherApp.git
-
-
 # config
 git config --global user.email "travis"
 git config --global user.name "travis CI deploy"
 
 # deploy
 cd build
-git init
 git add .
 git commit -m "Deployed to Github Pages. Build number: $TRAVIS_JOB_NUMBER"
-git push --quiet $REPO master:gh-pages 
+git push --quiet 
 echo "Successfully pushed changes to GitHub Pages"
