764afaa016f6223c2c90c8f7558a46f965abc6f1
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 14:49:47 +0300

    added new line to check existing of branch

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -5,6 +5,7 @@ set -o errexit #abort if any command fails
 mkdir build
 cd build
 git clone https://github.com/yarostbaklajana/WeatherApp.git
-git checkout gh-pages 
+git show-ref
+git checkout -t -b gh-pages origin/gh-pages
 rm -type f
 rm !(.git) -type c
