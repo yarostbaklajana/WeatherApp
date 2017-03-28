ec468dfd4866d1aa25e68b09e31b4cc61e835747
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-28 15:56:57 +0300

    Fixed before_build script

--- scripts/before_build.sh
+++ scripts/before_build.sh
@@ -4,8 +4,6 @@ set -o errexit #abort if any command fails
 
 mkdir build
 cd build
-git clone https://github.com/yarostbaklajana/WeatherApp.git
-git show-ref
-git checkout origin/gh-pages
-rm -type f
-rm !(.git) -type c
+git clone https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/WeatherApp.git .
+git checkout gh-pages
+
