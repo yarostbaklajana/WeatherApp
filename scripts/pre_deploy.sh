98565d3644873081f240bdc307cfed53cc7f113a
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-29 17:39:59 +0300

    Changed again

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -o errexit #abort if any command fails
-if [ "$TRAVIS_PULL_REQUEST" == "false" -o "$TRAVIS_BRANCH" == "gh-pages" ]
+if [ "$TRAVIS_PULL_REQUEST" == "true" -o "$TRAVIS_BRANCH" == "gh-pages" ]
 then 
     echo "Skipping a pre-deployment with the script provider because the current build is a pull request or gh-pages."
 else 
