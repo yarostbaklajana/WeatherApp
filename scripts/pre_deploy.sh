b0fd4943f658b42d67c18589874a090d61d3e313
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-03-29 17:38:35 +0300

    Changed condition of pull request

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -o errexit #abort if any command fails
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "gh-pages" ]
+if [ "$TRAVIS_PULL_REQUEST" == "false" -o "$TRAVIS_BRANCH" == "gh-pages" ]
 then 
+    echo "Skipping a pre-deployment with the script provider because the current build is a pull request or gh-pages."
+else 
     mkdir build
     cd build
     git clone https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/WeatherApp.git .
     git checkout gh-pages
     ls | grep -v .git | grep -v CNAME | xargs rm -rf
     cd ..
-else 
-    echo "Skipping a pre-deployment with the script provider because the current build is a pull request or gh-pages."
+    
 fi
