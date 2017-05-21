b2f52e0b9656b7030e20f9a480f3ec6e32ef31bc
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2017-05-21 08:28:37 +0300

    Added cancelRequest function to cancel requests that in process

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
 import SearchForm from './searchForm';
 import WeatherResults from './weatherResults';
+import Spinner from './spinner';
 import axios from 'axios';
 import moment from 'moment';
+
+const CancelToken = axios.CancelToken;
+let cancelRequest;
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             forecasts: [],
-            error: null
+            error: null,
+            isLoading: false
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async handleSubmit(cityId) {
+        
+        if (this.state.isLoading) {
+            cancelRequest();
+        }
+
+        this.setState({
+            isLoading: true,
+            forecasts: []
+        });
+
         try {
+
             this.setState({
                 forecasts: await this.getForecast(cityId),
-                error: null
-            }); 
+                error: null,
+                isLoading: false
+            });
+
+
+
         } catch (err) {
             this.setState({
                 error: err.message
             });
         }
-        
+
     }
 
     async getForecast(cityId) {
@@ -36,9 +56,11 @@ class App extends React.Component {
                     id: cityId,
                     APPID: process.env.OPENWEATHER_API_KEY,
                     units: 'metric'
-                }
+                },
+
+                cancelToken: new CancelToken((c) => { cancelRequest = c })
             });
-            
+
             const threeDayForecastList = fiveDayForecast.data.list.slice(0, 24);
 
             return this.getForecastCards(threeDayForecastList);
@@ -49,7 +71,8 @@ class App extends React.Component {
 
     getForecastCards(allForecastedDataArray) {
         const forecastCards = [];
-        allForecastedDataArray.forEach((singleForecast) => { forecastCards.push({
+        allForecastedDataArray.forEach((singleForecast) => {
+            forecastCards.push({
                 dateTime: this.convertToLocaleDateTime(singleForecast.dt),
                 weather: singleForecast.weather[0].description,
                 weatherIconUrl: `http://openweathermap.org/img/w/${singleForecast.weather[0].icon}.png`,
@@ -60,13 +83,14 @@ class App extends React.Component {
                     direction: this.convertToCompassDirection(singleForecast.wind.deg),
                     speed: singleForecast.wind.speed.toFixed(1)
                 }
-            }) });
-            return forecastCards;
+            })
+        });
+        return forecastCards;
     }
 
     convertToLocaleDateTime(utc) {
         const dateTime = moment.unix(utc);
-        return dateTime.format('DD/MM/YYYY - HH:mm'); 
+        return dateTime.format('DD/MM/YYYY - HH:mm');
     }
 
     convertHectopascalsToMmHg(hPa) {
@@ -98,7 +122,8 @@ class App extends React.Component {
             <div className='app-container'>
                 <header className='main-header'><h1 className='main-header_heading'>Weather</h1></header>
                 <SearchForm handleSubmit={this.handleSubmit} />
-                <div className='wather-cards-container'>{this.state.error ? this.state.error : <WeatherResults forecasts={this.state.forecasts} />}</div>
+
+                <div className='wather-cards-container'>{this.state.isLoading && <Spinner />}{this.state.error ? this.state.error : <WeatherResults forecasts={this.state.forecasts} />}</div>
             </div>
         )
     }
